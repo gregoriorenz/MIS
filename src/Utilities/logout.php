32ee059d1560f70015a5440f06b0ea11ba4a9bff
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-04 13:28:46 +0800

    Added CSS

--- src/Utilities/logout.php
+++ src/Utilities/logout.php
@@ -1,2 +1,17 @@
-<? 
-php?>
+<?php
+
+    session_start();
+
+    $_SESSION = array();
+
+    if(ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() -42000, 
+                  $params["path"], $params["domain"], 
+                  $param["secure"], $params["httponly"]);
+    }
+
+    session_destroy();
+    header("Location:/");
+    die();
+?>
